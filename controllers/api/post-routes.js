22168ea979188f27e7b505a60a9349a1869df98a
nicholsz <nichols.zane16@gmail.com>
2023-01-05 16:25:54 -0600

    working post page and comments shown

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -13,21 +13,22 @@ router.get('/:id', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['user_name', 'level'],
+          attributes: ['user_name', 'level', 'first_name', 'last_name', 'pic' ],
         },
         {
           model: Comment,
-          attributes: ['text'],
+          attributes: ['text', 'created_at'],
           include: {
             model: User,
-            attributes: ['user_name'],
+            attributes: ['user_name', 'level', 'first_name', 'last_name', 'pic' ],
           },
         },
       ],
     });
     const postData = getPost.get({ plain: true });
     console.log(postData);
-    res.status(200).json(getPost);
+    res.render('postpage', {postData});
+    // res.status(200).json(getPost);
   } catch (err) {
     console.log(err);
   }
